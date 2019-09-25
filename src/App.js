93d005c9d92491b68edf72408cf69d5739aae0b1
JulieChoJulie <h32cho@gmail.com>
2019-09-25 14:07:50 -0700

    news viewer api connected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
+require('dotenv').config();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [data, setData] = useState(null);
+    const YOUR_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+    const onClick = async () => {
+        try {
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=google-news-ca&apiKey=${YOUR_API_KEY}`);
+            setData(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <button onClick={onClick}>GET</button>
+            </div>
+            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+        </div>
+    );
+};
 
 export default App;
