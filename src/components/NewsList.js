e096bb6899a1b88ab731bc2073569bd23c3ded9b
JulieChoJulie <h32cho@gmail.com>
2019-10-15 15:32:22 -0400

    usePromise(customer hook) added

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 require('dotenv').config();
 
 
@@ -26,31 +27,30 @@ const sampleArticle = {
 }
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
     const YOUR_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=ca${query}&apiKey=${YOUR_API_KEY}`);
-                setArticles(response.data.articles);
-            } catch (e) {
-                console.log(e);
-            };
-            setLoading(false);
-        };
-        fetchData();
+
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=ca${query}&apiKey=${YOUR_API_KEY}`
+        );
     }, [category, YOUR_API_KEY]);
+
 
     if (loading) {
         return <NewsListBlock>Loading...</NewsListBlock>
     }
-    if (!articles) {
+    if (!response) {
         return null;
     }
+    if (error) {
+        return <NewsListBlock>Error has occurred....</NewsListBlock>
+    }
+
+    // If response is valid,
+    const { articles } = response.data;
+
 
     return (
         <NewsListBlock>
